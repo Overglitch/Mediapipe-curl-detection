31e4bf2f1166dbae618a02dbc5544b6093ea5f48
Overglitch_ <paul.murillo.d@uni.pe>
2023-07-05 23:58:45 -0500

    Update curl.py

--- curl.py
+++ curl.py
@@ -15,114 +15,6 @@ def calculate_angle(a, b, c):
 
     return angle
 
-
-cap = cv2.VideoCapture(0)
-
-# Curl counter variables
-counter = 0
-stage = None
-previous_stage = None
-
-## Setup mediapipe instance
-mp_drawing = mp.solutions.drawing_utils
-mp_pose = mp.solutions.pose
-
-with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
-    while cap.isOpened():
-        ret, frame = cap.read()
-
-        # Recolor image to RGB
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        image.flags.writeable = False
-
-        # Make detection
-        results = pose.process(image)
-
-        # Recolor back to BGR
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-        # Extract landmarks
-        try:
-            landmarks = results.pose_landmarks.landmark
-
-            # Get coordinates
-            shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
-                        landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
-            elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,
-                     landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
-            wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
-                     landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
-
-            # Calculate angle
-            angle = calculate_angle(shoulder, elbow, wrist)
-
-            # Visualize angle
-            cv2.putText(image, str(angle),
-                        tuple(np.multiply(elbow, [640, 480]).astype(int)),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
-
-            # Curl counter logic
-            if angle > 160:
-                stage = "down"
-            if angle < 30 and stage == 'down' and previous_stage != 'up':
-                stage = "up"
-                counter += 1
-                print(counter)
-
-            previous_stage = stage
-
-        except:
-            pass
-
-        # Render curl counter
-        # Setup status box
-        cv2.rectangle(image, (0, 0), (225, 73), (245, 117, 16), -1)
-
-        # Rep data
-        cv2.putText(image, 'REPS', (15, 12),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-        cv2.putText(image, str(counter),
-                    (10, 60),
-                    cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
-
-        # Stage data
-        cv2.putText(image, 'STAGE', (65, 12),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-        cv2.putText(image, stage,
-                    (60, 60),
-                    cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
-
-        # Render detections
-        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                  mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
-                                  mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
-                                  )
-
-        # Display image
-        cv2.imshow('Mediapipe Feed', image)
-
-        # Break the loop
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
-
-import mediapipe as mp
-import numpy as np
-
-def calculate_angle(a, b, c):
-    a = np.array(a)  # First
-    b = np.array(b)  # Mid
-    c = np.array(c)  # End
-
-    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
-    angle = np.abs(radians * 180.0 / np.pi)
-
-    if angle > 180.0:
-        angle = 360 - angle
-
-    return angle
-
-
 # Curl counter variables
 counter = 0
 stage = None
@@ -170,14 +62,14 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
 
             # Visualize angle
             cv2.putText(image, str(angle),
-                        tuple(np.multiply(elbow, [640, 480]).astype(int)),
+                        tuple(np.multiply(elbow, [image.shape[1], image.shape[0]]).astype(int)),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
 
             # Curl counter logic
             if angle > 160:
-                stage = "down"
-            if angle < 30 and stage == 'down' and previous_stage != 'up':
-                stage = "up"
+                stage = "abajo"
+            if angle < 30 and stage == 'abajo' and previous_stage != 'arriba':
+                stage = "arriba"
                 counter += 1
                 print(counter)
 
@@ -187,22 +79,24 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
             pass
 
         # Render curl counter
-        # Setup status box
-        cv2.rectangle(image, (0, 0), (225, 73), (245, 117, 16), -1)
+        background_width = 250
+        background_height = 100
+        text_offset_x = 20
+        text_offset_y = 100
 
         # Rep data
-        cv2.putText(image, 'REPS', (15, 12),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+        cv2.putText(image, 'REPETICIONES:', (image.shape[1] - background_width + text_offset_x, text_offset_y),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
         cv2.putText(image, str(counter),
-                    (10, 60),
-                    cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+                    (image.shape[1] - background_width + text_offset_x, text_offset_y + 40),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
         # Stage data
-        cv2.putText(image, 'STAGE', (65, 12),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+        cv2.putText(image, 'ESTADO:', (image.shape[1] - background_width + text_offset_x, text_offset_y + 80),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
         cv2.putText(image, stage,
-                    (60, 60),
-                    cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+                    (image.shape[1] - background_width + text_offset_x, text_offset_y + 120),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
         # Render detections
         mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
@@ -211,7 +105,7 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
                                   )
 
         # Display image
-        cv2.imshow('Mediapipe Feed', image)
+        cv2.imshow('ACTIVIDAD MEDIAPIPE', image)
 
         # Break the loop
         if cv2.waitKey(10) & 0xFF == ord('q'):
@@ -220,5 +114,9 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
 cap.release()
 cv2.destroyAllWindows()
 
-
-cap.release()
+#REFERENCIAS
+"""
+https://github.com/nicknochnack/MediaPipePoseEstimation
+https://youtu.be/0zhvUV1bAVQ
+código de clase de la semana 13
+"""
